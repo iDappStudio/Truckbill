2cde582a925040f48d356d67f0828bcc8bbfbe61
OskarPerliński <159592048+OskarPerlinski@users.noreply.github.com>
2025-03-03 19:57:00 +0100

    (tb-17) feat: Integrate Firebase with Flutter and update main application ent… (#9)
    feat: Integrate Firebase with Flutter and update main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:truckbill/core/di/di.dart';
+import 'package:truckbill/firebase_options.dart';
 import 'package:truckbill/presentation/router/app_router.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupDependencies();
   runApp(const MyApp());
 }
@@ -14,9 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       routerConfig: AppRouter.router,
     );
   }
