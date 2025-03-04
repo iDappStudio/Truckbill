c61b995a302c4593912a372355cd5590becc0a72
OskarPerliński <oskar.perlinskii@gmail.com>
2025-03-04 18:56:00 +0100

    feat: Refactor ShippingLetterDto and ShippingLetter models with updated fields and types

---
+++ lib/data/shipping_letter/dtos/shipping_letter_dto.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'shipping_letter_dto.freezed.dart';
+part 'shipping_letter_dto.g.dart';
+
+@freezed
+abstract class ShippingLetterDto with _$ShippingLetterDto {
+  factory ShippingLetterDto({
+    required String letterNumber,              
+    required String loadingPlace,               
+    required DateTime loadingDate,             
+    required String unloadingPlace,             
+    required DateTime unloadingDate,            
+    required double price,                  
+    required List<String> documentScnas,
+  }) = _ShippingLetterDto;
+
+  factory ShippingLetterDto.fromJson(Map<String, dynamic> json) => _$ShippingLetterDtoFromJson(json);
+}
