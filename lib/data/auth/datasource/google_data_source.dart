71c1897eac989b2646c63a9b43a8b5a86dfaf40d
OskarPerliński <oskar.perlinskii@gmail.com>
2025-03-09 16:01:36 +0100

    feat: Refactor authentication to separate Google and Apple data sources

--- lib/data/auth/datasource/google_data_source.dart
+++ lib/data/auth/datasource/google_data_source.dart
@@ -1,25 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
+ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:injectable/injectable.dart';
 
-@injectable
-class AuthDataSource {
+@singleton
+class GoogleAuthDataSource {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<UserCredential> signInWithApple() async {
-    final AuthorizationCredentialAppleID appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [AppleIDAuthorizationScopes.email, AppleIDAuthorizationScopes.fullName],
-    );
-
-    final OAuthProvider oAuthProvider = OAuthProvider('apple.com');
-    final AuthCredential credential = oAuthProvider.credential(
-      idToken: appleCredential.identityToken,
-      accessToken: appleCredential.authorizationCode,
-    );
-
-    return await _auth.signInWithCredential(credential);
-  }
 
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignIn googleSignIn = GoogleSignIn();
