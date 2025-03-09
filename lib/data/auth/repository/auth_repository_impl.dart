d8164a9adfdc8ff9e670670cce1ce8b2c4d5d983
OskarPerliński <oskar.perlinskii@gmail.com>
2025-03-09 15:34:07 +0100

    feat: Refactor authentication data sources into a single AuthDataSource class

--- lib/data/auth/repository/auth_repository_impl.dart
+++ lib/data/auth/repository/auth_repository_impl.dart
@@ -1,23 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
-import 'package:truckbill/data/auth/datasource/apple_data_source.dart';
-import 'package:truckbill/data/auth/datasource/google_data_source.dart';
+import 'package:truckbill/data/auth/datasource/auth_data_source.dart';
 import 'package:truckbill/domain/auth/repository/auth_repository.dart';
 
 @injectable
 class AuthRepositoryImpl implements AuthRepository {
-  final GoogleAuthDataSource _googleAuthDataSource;
-  final AppleAuthDataSource _appleAuthDataSource;
+  AuthRepositoryImpl(this._authDataSource);
 
-  AuthRepositoryImpl(this._googleAuthDataSource, this._appleAuthDataSource);
+  final AuthDataSource _authDataSource;
 
   @override
   Future<UserCredential> signInWithGoogle() async {
-    return _googleAuthDataSource.signInWithGoogle();
+    return _authDataSource.signInWithGoogle();
   }
 
   @override
   Future<UserCredential> signInWithApple() async {
-    return _appleAuthDataSource.signInWithApple();
+    return _authDataSource.signInWithApple();
   }
 }
