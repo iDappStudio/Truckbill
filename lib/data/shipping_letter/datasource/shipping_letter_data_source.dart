6f0499238fc0517c6d13cc92e499adc533e49cb5
OskarPerliński <oskar.perlinskii@gmail.com>
2025-03-14 14:00:29 +0100

    feat: Refactor ShippingLetterDataSource to use Firestore converters for improved data handling

--- lib/data/shipping_letter/datasource/shipping_letter_data_source.dart
+++ lib/data/shipping_letter/datasource/shipping_letter_data_source.dart
@@ -2,13 +2,21 @@ import 'package:injectable/injectable.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:truckbill/data/shipping_letter/dtos/shipping_letter_dto.dart';
 
-@Singleton()
+@singleton
 class ShippingLetterDataSource {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> createShippingLetter(ShippingLetterDto dto, String userId) async {
     try {
-      await _firestore.collection('users').doc(userId).collection('documents').add(dto.toJson());
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('documents')
+          .withConverter<ShippingLetterDto>(
+            fromFirestore: (snapshot, _) => ShippingLetterDto.fromJson(snapshot.data()!),
+            toFirestore: (dto, _) => dto.toJson(),
+          )
+          .add(dto);
     } catch (e) {
       throw Exception("Failed to create shipping letter.");
     }
@@ -16,26 +24,44 @@ class ShippingLetterDataSource {
 
   Future<List<ShippingLetterDto>> getShippingLetter(String userId) async {
     try {
-      final querySnapshot = await _firestore.collection('users').doc(userId).collection('documents').get();
-      return querySnapshot.docs.map((doc) {
-        return ShippingLetterDto.fromJson(doc.data());
-      }).toList();
+      final querySnapshot =
+          await _firestore
+              .collection('users')
+              .doc(userId)
+              .collection('documents')
+              .withConverter<ShippingLetterDto>(
+                fromFirestore: (snapshot, _) => ShippingLetterDto.fromJson(snapshot.data()!),
+                toFirestore: (dto, _) => dto.toJson(),
+              )
+              .get();
+
+      return querySnapshot.docs.map((doc) => doc.data()).toList();
     } catch (e) {
-      throw Exception("Failed to get shipping letters.");
+      throw Exception("Failed to fetch shipping letters.");
     }
   }
 
   Future<ShippingLetterDto> getShippingLetterById(String id, String userId) async {
     try {
-      final doc = await _firestore.collection('users').doc(userId).collection('documents').doc(id).get();
+      final doc =
+          await _firestore
+              .collection('users')
+              .doc(userId)
+              .collection('documents')
+              .withConverter<ShippingLetterDto>(
+                fromFirestore: (snapshot, _) => ShippingLetterDto.fromJson(snapshot.data()!),
+                toFirestore: (dto, _) => dto.toJson(),
+              )
+              .doc(id)
+              .get();
 
-      if (doc.data() == null) {
+      if (!doc.exists) {
         throw Exception("Shipping letter not found.");
       }
 
-      return ShippingLetterDto.fromJson(doc.data() as Map<String, dynamic>);
+      return doc.data()!;
     } catch (e) {
-      throw Exception("Failed to get shipping letter.");
+      throw Exception("Failed to fetch shipping letter.");
     }
   }
 }
